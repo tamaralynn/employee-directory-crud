15260814bc9ec99fc70fbe1eca6d19a55850a760
Tami Shute <tamaralynnrenee@gmail.com>
2020-06-07 00:16:43 -0400

    all employees query works

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "root",
     database: "employeesdb"
   });
 
@@ -27,12 +27,22 @@ const connection = mysql.createConnection({
 
       clear();
       console.log(ascii_text)
+
       initialPrompt();
       
   });
 
 }
-console.log(ascii_text);
+
+function renderTable(tableTitle, tableData){
+    clear();
+
+    console.log(chalk.inverse.bold(tableTitle));
+   
+    console.table(tableData);
+    
+    initialPrompt();
+}
 
 function initialPrompt(){
     inquirer
@@ -188,7 +198,7 @@ function addEmployee(){
                 choices: rolesTitles
               })
             .then(answer => {
-                //get id of selected role
+                
                 const selectedRole = answer.rolePrompt;
                 let selectedRoleID;
                 for (let i = 0; i < roles.length; i++) {
@@ -254,5 +264,37 @@ function addEmployee(){
         });            
     });
 }
+
+
+function queryEmployeesAll(){
+
+    const query = `
+    SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department_name, concat(manager.first_name, " ", manager.last_name) AS manager_full_name
+    FROM employee 
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON department.id = role.department_id
+	LEFT JOIN employee as manager ON employee.manager_id = manager.id;`;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+
+        const tableData = [];
+        for (let i = 0; i < res.length; i++) {
+            tableData.push({ 
+                "ID": res[i].id, 
+                "First Name": res[i].first_name,
+                "Last Name": res[i].last_name,
+                "Role": res[i].title,
+                "Salary": res[i].salary, 
+                "Department": res[i].department_name,
+                "Manager": res[i].manager_full_name
+            });
+        }
+        
+        renderTable("All Employees", tableData);
+    });
+}
+
+
+
 
 init();
